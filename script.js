999ac6a9e7dc67800091053bd01b5e0ce87aacf7
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-10 03:58:26 +0200

    Add Clr Btn, Basic functionality
    - Comma can only be pressed once
    - Display is showing correct numbers
    - Operator doesn't change if numTwo exists
    
    TO-DO
    
    - Create operate(numTwo,numTwo,operator)
    - Limit characters allowed onscreen

--- script.js
+++ script.js
@@ -2,6 +2,83 @@
 const body = document.querySelector("body");
 const bntContainer = document.querySelector(".btns-container");
 const buttons = document.querySelectorAll(".btns");
+const display = document.querySelector(".display");
+
+// ================= VARIABLES =====================
+
+let numOne = "";
+// console.log(numOne);
+
+let operator = "";
+// console.log(operator);
+
+let numTwo = "";
+
+display.textContent = numOne;
+
+// ============= FUNCTIONALITY =========================
+
+buttons.forEach((x) =>
+  x.addEventListener("click", (e) => {
+    const button = e.target;
+    // **** CLEAR ****
+    if (button.id == "clear") {
+      numOne = "";
+      numTwo = "";
+      operator = "";
+      display.textContent = 0;
+    } else if (button.id == "divide") {
+      if (numTwo == "") {
+        operator = button.id;
+      }
+    } else if (button.id == "multiply") {
+      if (numTwo == "") {
+        operator = button.id;
+      }
+    } else if (button.id == "subtract") {
+      if (numTwo == "") {
+        operator = button.id;
+      }
+    } else if (button.id == "add") {
+      if (numTwo == "") {
+        operator = button.id;
+      }
+    } else if (button.id == "comma") {
+      // **** COMMA ****;
+      if (numTwo == "") {
+        if (!numOne.includes(".")) {
+          numOne += ".";
+          display.textContent = numOne;
+        }
+      } else {
+        if (!numTwo.includes(".")) {
+          numTwo += ".";
+          display.textContent = numTwo;
+        }
+      }
+    } else if (button.id == "operator") {
+      // **** OPERATOR ****
+      console.log(numOne);
+      console.log(numTwo);
+      console.log(operator);
+    } else {
+      // **** DIGIT BTNS ****
+      // IF numTwo IS ""
+      if (numTwo == "" && !operator) {
+        numOne += button.id;
+        display.textContent = numOne;
+      } else {
+        numTwo += button.id;
+        display.textContent = numTwo;
+      }
+      // console.log(display.textContent);
+    }
+
+    // console.log(button.id);
+  })
+);
+
+// console.log(buttons);
 
 // ================ BUTTON EFFECTS =================
 
@@ -20,13 +97,3 @@ document.addEventListener("mouseup", () => {
     button.setAttribute("style", "box-shadow: 0");
   }
 });
-
-// ============= FUNCTIONALITY =========================
-
-buttons.forEach((x) =>
-  x.addEventListener("click", (e) => {
-    // console.log(e.target.style);
-  })
-);
-
-// console.log(buttons);
