8754a966173dda1b4d89ebee502ea2e962b53533
k4ileb <40836142+k4iLeb@users.noreply.github.com>
2025-01-12 19:47:00 +0200

    Finish project #2
    - Fixed Display border-radius
    - Added Keyboard Support
    - Fixed spaghetti code for showing active selector

--- script.js
+++ script.js
@@ -7,11 +7,7 @@ const display = document.querySelector(".display");
 // ================= VARIABLES =====================
 
 let numOne = 0;
-// console.log(numOne);
-
 let operator = "";
-// console.log(operator);
-
 let numTwo = "";
 let result = "";
 display.textContent = numOne;
@@ -28,6 +24,7 @@ buttons.forEach((x) =>
       operator = "";
       result = "";
       display.textContent = numOne;
+      checkOperator();
     } else if (button.id == "divide") {
       if (numTwo == "") {
         operator = button.id;
@@ -35,6 +32,7 @@ buttons.forEach((x) =>
         operate(numOne, numTwo, operator);
         continueCalcs(button.id);
       }
+      checkOperator();
     } else if (button.id == "multiply") {
       if (numTwo == "") {
         operator = button.id;
@@ -42,6 +40,7 @@ buttons.forEach((x) =>
         operate(numOne, numTwo, operator);
         continueCalcs(button.id);
       }
+      checkOperator();
     } else if (button.id == "subtract") {
       if (numTwo == "") {
         operator = button.id;
@@ -49,6 +48,7 @@ buttons.forEach((x) =>
         operate(numOne, numTwo, operator);
         continueCalcs(button.id);
       }
+      checkOperator();
     } else if (button.id == "add") {
       if (numTwo == "") {
         operator = button.id;
@@ -56,6 +56,7 @@ buttons.forEach((x) =>
         operate(numOne, numTwo, operator);
         continueCalcs(button.id);
       }
+      checkOperator();
     } else if (button.id == "comma") {
       // **** COMMA ****;
       if (numTwo == "" && !operator) {
@@ -81,6 +82,7 @@ buttons.forEach((x) =>
 
       operate(numOne, numTwo, operator);
       operator = "";
+      checkOperator();
     } else if (button.id == "backspace") {
       // **** BACKSPACE ****
       if (numOne && !operator && !result) {
@@ -100,22 +102,135 @@ buttons.forEach((x) =>
             numOne.toString().startsWith("0") &&
             !numOne.toString().includes(".")
           ) {
-            numOne = button.id;
+            numOne = button.value;
             display.textContent = numOne;
           } else {
-            numOne += button.id;
+            numOne += button.value;
             display.textContent = numOne;
           }
         }
       } else if (operator) {
         if (numTwo.toString().length < 8) {
-          numTwo += button.id;
+          numTwo += button.value;
           display.textContent = numTwo;
         }
       }
     }
   })
 );
+
+// **** KEYBOARD SUPPORT ****
+
+document.addEventListener("keypress", (e) => {
+  // **** DIGITS ****
+  if (/[0-9]/.test(e.key)) {
+    if (numTwo == "" && !operator && !result) {
+      if (numOne.toString().length < 8) {
+        if (
+          numOne.toString().startsWith("0") &&
+          !numOne.toString().includes(".")
+        ) {
+          numOne = e.key;
+          display.textContent = numOne;
+        } else {
+          numOne += e.key;
+          display.textContent = numOne;
+        }
+      }
+    } else if (operator) {
+      if (numTwo.toString().length < 8) {
+        numTwo += e.key;
+        display.textContent = numTwo;
+      }
+    }
+  }
+  // **** COMMA ****
+  if (/[.]/.test(e.key)) {
+    if (numTwo == "" && !operator) {
+      if (numOne == 0) {
+        numOne = "0.";
+        display.textContent = numOne;
+      }
+      if (!numOne.includes(".")) {
+        numOne += ".";
+        display.textContent = numOne;
+      }
+    } else if (operator) {
+      if (!numTwo.includes(".")) {
+        numTwo += ".";
+        display.textContent = numTwo;
+      }
+    }
+  }
+  // **** OPERATE ****
+  if (e.key == "=" || e.key == "Enter") {
+    if (numTwo == "" || !numTwo) {
+      return;
+    }
+
+    operate(numOne, numTwo, operator);
+    operator = "";
+    checkOperator();
+  }
+  // **** CALCS ****
+  if (/[-+/*]/.test(e.key)) {
+    if (e.key == "/") {
+      if (numTwo == "") {
+        operator = "divide";
+      } else {
+        operate(numOne, numTwo, operator);
+        continueCalcs("divide");
+      }
+    } else if (e.key == "*") {
+      if (numTwo == "") {
+        operator = "multiply";
+      } else {
+        operate(numOne, numTwo, operator);
+        continueCalcs("multiply");
+      }
+    } else if (e.key == "-") {
+      if (numTwo == "") {
+        operator = "subtract";
+      } else {
+        operate(numOne, numTwo, operator);
+        continueCalcs("subtract");
+      }
+    } else if (e.key == "+") {
+      if (numTwo == "") {
+        operator = "add";
+      } else {
+        operate(numOne, numTwo, operator);
+        continueCalcs("add");
+      }
+    }
+    checkOperator();
+  }
+  // **** CLEAR ****
+  if (e.key == "c") {
+    numOne = 0;
+    numTwo = "";
+    operator = "";
+    result = "";
+    display.textContent = numOne;
+    checkOperator();
+  }
+});
+
+// **** BACKSPACE ****
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Backspace") {
+    if (numOne && !operator && !result) {
+      numOne = numOne.slice(0, numOne.length - 1);
+      display.textContent = numOne;
+    }
+    if (numTwo && operator) {
+      numTwo = numTwo.slice(0, numTwo.length - 1);
+      display.textContent = numTwo;
+    }
+  }
+});
+
+// **** FUNCTIONS ****
 
 function operate(numOne, numTwo, operator) {
   numOne = Number(numOne);
@@ -148,57 +263,179 @@ function continueCalcs(button) {
   operator = button;
 }
 
+// **** CHANGE COLOR FOR SELECTED OPERATOR *****
+function checkOperator() {
+  let btns = document.querySelectorAll(".calc-btns");
+  if (operator) {
+    btns.forEach((button) => (button.style.backgroundColor = "#EBCB64"));
+    let button = document.querySelector(`#${operator}`);
+    button.style.backgroundColor = "#DBB539";
+  } else {
+    btns.forEach((button) => (button.style.backgroundColor = "#EBCB64"));
+  }
+}
+
 // ================ BUTTON EFFECTS =================
 // **** MOUSEDOWN & MOUSEUP ANIMATIONS ****
 buttons.forEach((x) =>
   x.addEventListener("mousedown", (e) => {
     const button = e.target;
-    // console.log(button.classList);
-    if (!button.classList.contains("calc-btns")) {
-      // console.log("hi");
-      button.setAttribute(
-        "style",
-        "box-shadow: inset 0px 0px 5px rgb(53, 52, 52)"
-      );
-    } else {
-      button.setAttribute(
-        "style",
-        "box-shadow: inset 0px 0px 5px rgb(53, 52, 52); background-color: rgb(226, 179, 26)"
-      );
-    }
+    button.style.boxShadow = "inset 0px 0px 5px rgb(53, 52, 52)";
   })
 );
 
 document.addEventListener("mouseup", () => {
   for (let button of buttons) {
-    if (!button.classList.contains("calc-btns")) {
-      button.setAttribute("style", "box-shadow: 0");
-    } else {
-      if (operator != button.id) {
-        button.setAttribute(
-          "style",
-          "box-shadow: 0; background-color: #f1c956"
-        );
-      }
-    }
+    button.style.boxShadow = "none";
   }
 });
 
-// **** TODO => CHANGE COLOR FOR SELECTED OPERATOR *****
-const calcBtns = [...buttons].filter(
-  (x) =>
-    x.classList.contains("calc-btns") || x.classList.contains("operator-btn")
-);
+// ================ KEYBOARD EFFECTS =================
 
-calcBtns.forEach((button) =>
-  button.addEventListener("click", (e) => {
-    calcBtns.forEach((x) => {
-      if (x.id != "operator") {
-        x.setAttribute("style", "box-shadow: 0; background-color: #f1c956");
-      }
-    });
-    if (button.id != "operator") {
-      e.target.style.backgroundColor = "rgb(226, 179, 26)";
+document.addEventListener("keydown", (e) => {
+  if (/[0-9/+\-=*\.]/.test(e.key)) {
+    let value;
+    switch (e.key) {
+      case ".":
+        value = "comma";
+        break;
+      case "0":
+        value = "zero";
+        break;
+      case "1":
+        value = "one";
+        break;
+      case "2":
+        value = "two";
+        break;
+      case "3":
+        value = "three";
+        break;
+      case "4":
+        value = "four";
+        break;
+      case "5":
+        value = "five";
+        break;
+      case "6":
+        value = "six";
+        break;
+      case "7":
+        value = "seven";
+        break;
+      case "8":
+        value = "eight";
+        break;
+      case "9":
+        value = "nine";
+        break;
+      case "Backspace":
+        value = "backspace";
+        break;
+      case "-":
+        value = "subtract";
+        break;
+      case "+":
+        value = "add";
+        break;
+      case "*":
+        value = "multiply";
+        break;
+      case "/":
+        value = "divide";
+        break;
+      case "=":
+        value = "operator";
+        break;
+      case "c":
+        value = "clear";
+        break;
+      default:
+        break;
     }
-  })
-);
+    let button = document.querySelector(`#${value}`);
+    button.style.boxShadow = "inset 0px 0px 5px rgb(53, 52, 52)";
+  }
+  if (e.key == "Backspace") {
+    let button = document.querySelector("#backspace");
+    button.style.boxShadow = "inset 0px 0px 5px rgb(53, 52, 52)";
+  }
+  if (e.key == "Enter") {
+    let button = document.querySelector("#operator");
+    button.style.boxShadow = "inset 0px 0px 5px rgb(53, 52, 52)";
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  if (/[0-9/+\-=*\.]/.test(e.key)) {
+    let value;
+    switch (e.key) {
+      case ".":
+        value = "comma";
+        break;
+      case "0":
+        value = "zero";
+        break;
+      case "1":
+        value = "one";
+        break;
+      case "2":
+        value = "two";
+        break;
+      case "3":
+        value = "three";
+        break;
+      case "4":
+        value = "four";
+        break;
+      case "5":
+        value = "five";
+        break;
+      case "6":
+        value = "six";
+        break;
+      case "7":
+        value = "seven";
+        break;
+      case "8":
+        value = "eight";
+        break;
+      case "9":
+        value = "nine";
+        break;
+      case "Backspace":
+        value = "backspace";
+        break;
+      case "-":
+        value = "subtract";
+        break;
+      case "+":
+        value = "add";
+        break;
+      case "*":
+        value = "multiply";
+        break;
+      case "/":
+        value = "divide";
+        break;
+      case "=":
+        value = "operator";
+        break;
+      case "c":
+        value = "clear";
+        break;
+      default:
+        break;
+    }
+    let button = document.querySelector(`#${value}`);
+    button.style.boxShadow = "none";
+  }
+  if (e.key == "Backspace") {
+    let button = document.querySelector("#backspace");
+    button.style.boxShadow = "none";
+  }
+  if (e.key == "Enter") {
+    let button = document.querySelector("#operator");
+    button.style.boxShadow = "none";
+  }
+});
